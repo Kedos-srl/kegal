1c9dfa54308eb799a5092c16f48caf560344148b
fabio <fabio.gagliardi@kedos-srl.it>
2025-03-09 10:29:43 +0100

    rag test documents changed

--- tests/rag_test/test_rag.py
+++ tests/rag_test/test_rag.py
@@ -5,7 +5,7 @@ from kegal.llm.ollama_handler import OllamaHandler
 
 if __name__ == '__main__':
     response = compile_form_yaml_file(Path("rag_test.yml"),
-                                 message="scrivi una breve sintesti della produzione vinicola italiana degli ultimi anni")
+                                 message="scrivi una breve sintesti della produzione vinicola pugliese degli ultimi anni")
     for r in response:
         print(r)
 

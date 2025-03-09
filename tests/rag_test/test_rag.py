346ea08ac47f78b5582dbbee3297554089638a44
fabio <fabio.gagliardi@kedos-srl.it>
2025-03-09 01:53:03 +0100

    small models tests

---
+++ tests/rag_test/test_rag.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+from kegal.compile import  compile_form_yaml_file
+from kegal.llm.ollama_handler import OllamaHandler
+
+if __name__ == '__main__':
+    response = compile_form_yaml_file(Path("rag_test.yml"),
+                                 message="scrivi una breve sintesti della produzione vinicola italiana degli ultimi anni")
+    for r in response:
+        print(r)
+

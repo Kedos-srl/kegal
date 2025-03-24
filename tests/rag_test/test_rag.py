8ce2afbc06a9bf200c08a8dfeba32d8cfefca771
fabio <fabio.gagliardi@kedos-srl.it>
2025-03-25 00:02:41 +0100

    Refactor report generation and add validation tracking.

--- tests/rag_test/test_rag.py
+++ tests/rag_test/test_rag.py
@@ -5,16 +5,21 @@ from datetime import datetime
 from kegal.compile import compile_from_yaml_file
 from kegal.llm.ollama_handler import OllamaHandler
 from tests.rag_test.questions.questions import load_question_file_en, load_question_file_it
-from tests.utils.reports import save_markdown_report
 
 REPORT_PATH = Path(__file__).parent / "reports"
+
+def save_markdown(file_path: Path,   content: str):
+    # Create file path and save content
+    file_path.write_text(content)
+    print(f"File saved to {file_path}")
 
 
 if __name__ == '__main__':
     print("test rag")
     # Create directory with timestamp
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    dir_name = f"chat_{timestamp}"
+    report_dir = REPORT_PATH / f"report_{timestamp}"
+    report_dir.mkdir(parents=True, exist_ok=True)
 
 
     questions_en = load_question_file_en()["questions"]
@@ -25,6 +30,8 @@ if __name__ == '__main__':
     general_report = {
         "output_failed": 0,
         "output_ok": 0,
+        "validation_failed": 0,
+        "validation_ok": 0
     }
     reports = ""
     chats = ""
@@ -70,15 +77,25 @@ if __name__ == '__main__':
                 continue
 
             general_report["output_ok"] += 1
-            chats += "\n---\n"
-            reports += "\n---\n"
+        chats += "\n---\n"
+        reports += "\n---\n"
+
+        if validation == True and all(x == validation for x in chat_validations):
+            general_report["validation_ok"] += 1
+        elif validation == False and any(x == validation for x in chat_validations):
+            general_report["validation_ok"] += 1
+        else:
+            general_report["validation_failed"] += 1
+
 
 
     REPORT_PATH.mkdir(parents=True, exist_ok=True)
     report = f"## RESUME\n\n"
     report += f"- **output_failed**: {general_report['output_failed']}\n"
     report += f"- **output_ok**: {general_report['output_ok']}\n"
+    report += f"- **validation_failed**: {general_report['validation_failed']}\n"
+    report += f"- **validation_ok**: {general_report['validation_ok']}\n"
     reports = report + "\n\n" + f"## REPORTS\n\n" + reports
 
-    save_markdown_report(REPORT_PATH  / "reports", reports)
-    save_markdown_report(REPORT_PATH  / "chats", chats)
+    save_markdown(report_dir / "reports.md" ,reports)
+    save_markdown(report_dir / "chats.md" ,chats)
--- tests/utils/reports.py
+++ tests/utils/reports.py
@@ -2,13 +2,3 @@ from datetime import datetime
 from pathlib import Path
 
 
-def save_markdown_report(md_dir: Path, content: str):
-    # Create timestamp and directory name
-    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    # Ensure the directory exists
-    md_dir.mkdir(parents=True, exist_ok=True)
-
-    # Create file path and save content
-    file_path = md_dir / f"rag_test_{timestamp}.md"
-    file_path.write_text(content)
-    print(f"File saved to {file_path}")
